78d229ef1ac9ca95cf65d36c7d5a91894a86b934
Vera-poms <verapomaa99@gmail.com>
2025-01-02 00:02:54 +0000

    Deleted product from HTML when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
  console.log(matchingProduct)
 
  cartSummaryHTML += `
-   <div class="cart-item-container">
+   <div class="cart-item-container js-cart-item-container-${matchingProduct .id}">
       <div class="delivery-date">
         Delivery date: Tuesday, June 21
       </div>
@@ -98,11 +98,15 @@ cart.forEach((cartItem) => {
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML
 
+
+
 document.querySelectorAll('.js-delete-quantity-link').forEach((link) => {
  link.addEventListener('click', () => {
   const {productId} = link.dataset
   removeFromCart(productId) 
 
+  const container = document.querySelector(`.js-cart-item-container-${productId}`)
+  container.remove()
  })
 
 })
