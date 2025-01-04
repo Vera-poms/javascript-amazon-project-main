2ee2940dd16cdec49f97fc7d4dc9997da4dc3ee3
Vera-poms <verapomaa99@gmail.com>
2025-01-04 20:53:58 +0000

    Saved data and generated HTML for the summary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,11 +5,11 @@ import {
  updateQuantity,
  updateDeliveryOption 
 } from '../../data/cart.js'
-import {products} from '../../data/products.js'
+import {products, getProduct} from '../../data/products.js'
 import { formatCurrency } from '../utils/money.js'
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js' //uses a default export that is why it doesn't need the curly braces. However, the ones above use named exports
-import { deliveryOptions } from '../../data/deliveryOptions.js' 
+import { deliveryOptions, getDeliveryOption } from '../../data/deliveryOptions.js' 
 
 
 hello()
@@ -28,23 +28,11 @@ export function renderOrderSummary(){
 
  const productId = cartItem.productId 
 
- let matchingProduct
-
- products.forEach((product) => {
-   if(product.id === productId){
-   matchingProduct = product
-   }
- })
+ const matchingProduct = getProduct(productId)
 
  const {deliveryOptionId} = cartItem
 
- let deliveryOption
-
- deliveryOptions.forEach((option) => {
-   if(option.id === deliveryOptionId){
-     deliveryOption = option
-   }
- })
+ const deliveryOption = getDeliveryOption(deliveryOptionId)
 
  const today = dayjs()
  const deliveryDate = today.add(deliveryOption.deliveryDays, 'days')
