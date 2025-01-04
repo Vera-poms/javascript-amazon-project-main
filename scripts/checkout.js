df109b5f28835f9660f12bf049239ea314cc3b2a
Vera-poms <verapomaa99@gmail.com>
2025-01-04 19:16:50 +0000

    Made delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,13 +2,15 @@ import {
   cart, 
   removeFromCart, 
   calculateCartQuantity, 
-  updateQuantity
+  updateQuantity,
+  updateDeliveryOption 
 } from '../data/cart.js'
 import {products} from '../data/products.js'
 import { formatCurrency } from './utils/money.js'
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js'
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js' //uses a default export that is why it doesn't need the curly braces. However, the ones above use named exports
 import { deliveryOptions } from '../data/deliveryOptions.js' 
+
 
 hello()
 
@@ -109,7 +111,8 @@ let html = ''
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId
 
      html+=`
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option" data-product-id="${matchingProduct.id}"
+        data-delivery-option-id="${deliveryOption.id}">
             <input type="radio"
             ${isChecked ? 'checked': ''}
               class="delivery-option-input"
@@ -215,5 +218,10 @@ document.body.addEventListener('keydown', (event) =>{
 
 })
 
-
-
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const {productId, deliveryOptionId} = element.dataset
+     updateDeliveryOption(productId, deliveryOptionId)
+  })
+})
+ 
