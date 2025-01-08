7730d40c3d8d63ec304af77020d0d7c801e1590e
Vera-poms <verapomaa99@gmail.com>
2025-01-08 16:07:19 +0000

    Created automated tests for orderSummary

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -15,7 +15,7 @@ import { renderCheckoutHeader } from './checkoutHeader.js'
 
 export function renderOrderSummary(){
 
- updateCartQuantity()
+
  let cartSummaryHTML = ''
 
  cart.forEach((cartItem) => {
@@ -32,7 +32,7 @@ export function renderOrderSummary(){
 
 
  cartSummaryHTML += `
-   <div class="cart-item-container js-cart-item-container-${matchingProduct .id}">
+   <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct .id}">
        <div class="delivery-date">
          Delivery date: ${dateString}
        </div>
@@ -48,7 +48,7 @@ export function renderOrderSummary(){
            <div class="product-price">
              $${formatCurrency(matchingProduct.priceCents)}
            </div>
-           <div class="product-quantity">
+           <div class="product-quantity js-product-quantity-${matchingProduct.id}">
              <span>
                Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
              </span>
@@ -57,7 +57,7 @@ export function renderOrderSummary(){
              </span>
              <input class="quantity-input js-quantity-input-${matchingProduct.id}">
              <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
-             <span class="delete-quantity-link link-primary js-delete-quantity-link" data-product-id="${matchingProduct.id}">
+             <span class="delete-quantity-link link-primary js-delete-quantity-link js-delete-link-${matchingProduct.id}" data-product-id="${matchingProduct.id}">
                Delete
              </span>
            </div> 
@@ -122,9 +122,8 @@ export function renderOrderSummary(){
    renderOrderSummary()
    updateCartQuantity()
    renderPaymentSummary()
- })
-
- })
+  })
+})
 
  function updateCartQuantity(){
    const cartQuantity = calculateCartQuantity()
